e38e23f90379cf2dfcfc4edeb0d855a512cf6fac
«oleglust» <filslip@yandex.ru>
2022-08-09 11:25:28 +0300

    aaa

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//программа принимает число и выдает сумму чисел от 1 до этого числа
+int getSumNums(int number)
+{
+   int sum = 0;
+   while(number > 0)
+   {
+     sum += number;
+    number -= 1;
+   }
+   return sum;
+}
+
+
+
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+
+int answer = getSumNums(x);
+Console.WriteLine(answer);
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа получает число и указывает количество цифр в нем
+
+int Lenght(int num)
+{
+   int counter = 0;
+   while (num > 0)
+{
+   num = num / 10;
+   counter++;
+}
+
+return counter;
+
+}
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Lenght(x));
